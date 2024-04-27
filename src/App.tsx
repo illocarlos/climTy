f36c26b54254ea6e1be9108bf6300d6c4b66301b
illocarlos <carlosalbendiz93@gmail.com>
2024-04-28 00:04:26 +0200

    implement css modules and app.modules.cssn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
+import './App.modules.css'
 function App() {
 
   return (
     <>
-      <h1>hola mundo</h1>
+      <h1 className='title'>hola mundo</h1>
+      <div className='container'>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
